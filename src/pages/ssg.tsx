dc61db8b91878a31ea061f32c07d89f7e3bb13c9
Przemyslaw Kozinski <przemyslaw.kozinski@alan-systems.com>
2021-01-29 12:18:19 +0100

    added : axios and static client component

---
+++ src/pages/ssg.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
+
+interface Props { }
+
+const StaticSide = ( props: Props ) => {
+
+  
+  const [pokemon, setPokemon] =  useState<any[]>( [] );
+  
+  useEffect( () => {
+    const fetchPokemon = async () => {
+      const response = await axios.get( url );
+
+      const promises: any[] = [];
+      response.data.results.forEach( ( result: any ) => {
+        promises.push( axios.get( result.url ) );
+      } );
+
+      const responses = await Promise.all( promises );
+
+      const pokeData = responses.map( item => {
+        return {
+          name: item.data.name,
+          imgUrl: item.data.sprites.front_default,
+        }
+      } );
+      
+      setPokemon( pokeData );
+    };
+    fetchPokemon();
+    
+  }, [setPokemon] );
+
+  return pokemon.map( ( poke: any ) => (
+    <div key={poke.name}>
+      <img src={poke.imgUrl} alt={poke.name} />
+      <p>{poke.name}</p>
+      <hr />
+    </div>
+  ) );
+};
+
+export default StaticSide;
